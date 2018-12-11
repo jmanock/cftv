b65b317de631a32c49d1cc8af9237552b031b24a
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-12-11 11:08:03 -0500

    more style

--- src/Components/Layouts/Sections/Wwdaw.jsx
+++ src/Components/Layouts/Sections/Wwdaw.jsx
@@ -3,28 +3,31 @@ import {Grid,Paper} from '@material-ui/core';
 import greenPepper from '../../../images/greenPepppercopy.png';
 
 const Wwdaw = () =>{
+  const style={
+    margin:'20px',
+  }
   return(
     <section className='wwdaw' style={{height:'50vh'}}>
       <h1 className='wwdawTitle'>WHAT WE DO, AND WHY</h1>
       <Grid container>
-        <Grid item xs>
-          <Paper elevation={3} style={{height:'40vh', marginLeft:'20px'}}>
+        <Grid item xs style={style}>
+          <Paper elevation={3} style={{height:'35vh'}}>
             <img src={greenPepper} alt='greenPepper' className='pepper'></img>
             <ol className='list'>
               <li>Provide local resource information in a video magazine format</li>
             </ol>
           </Paper>
         </Grid>
-        <Grid item xs>
-          <Paper elevation={3} style={{height:'40vh'}}>
+        <Grid item xs style={style}>
+          <Paper elevation={3} style={{height:'35vh'}}>
             <img src={greenPepper} alt='Green Pepper' className='pepper'></img>
             <ol className='list'>
               <li>Provide a platform to advertise products, events, and locations to key known audience</li>
             </ol>
           </Paper>
         </Grid>
-        <Grid item xs>
-          <Paper elevation={3} style={{height:'40vh', marginRight:'20px'}}>
+        <Grid item xs style={style}>
+          <Paper elevation={3} style={{height:'35vh'}}>
             <img src={greenPepper} alt='Green Pepper' className='pepper'></img>
             <ol className='list'>
               <li>Create partnerships with key compainies who have a shared ethos</li>
