e3fe6d96498963ffe764f1356c12d558c4961fcd
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-12-13 17:57:21 -0500

    better footer

--- src/Components/Layouts/Footer.jsx
+++ src/Components/Layouts/Footer.jsx
@@ -14,11 +14,11 @@ const Footer = () =>{
           </div>
         </div>
       </div>
-      <div className='footer-copyright text-center' style={{backgroundColor:'#182236',color:'#fff'}}>@ 2018 Copyright: CycleFeverTv
-        <a href='https://www.facebook.com/CycleFeverTV/' target='_blank'> <i className='fab fa-facebook-f' style={{marginLeft:20,fontSize:30}}></i></a>
-      <a href='https://twitter.com/CycleFeverTV' target='_blank'> <i className='fab fa-twitter' style={{marginLeft:20,fontSize:30}}></i></a>
-      <a href='https://www.instagram.com/cyclefevertv/' target='_blank'><i className='fab fa-instagram' style={{marginLeft:20,fontSize:30}}></i></a>
-      <a href='https://www.youtube.com/channel/UC5lBCpUunSuN3lCxxTfkunA' target='_blank'><i className='fab fa-youtube' style={{marginLeft:20,fontSize:30}}></i></a>
+      <div className='footer-copyright text-center' style={{backgroundColor:'#182236',color:'#f6f1ed'}}>&copy; 2018 <span style={{color:'#963019'}}>CycleFeverTv</span>
+        <a href='https://www.facebook.com/CycleFeverTV/' target='_blank'> <i className='fab fa-facebook-f' style={{marginLeft:20,fontSize:30,color:'#667467'}}></i></a>
+      <a href='https://twitter.com/CycleFeverTV' target='_blank'> <i className='fab fa-twitter' style={{marginLeft:20,fontSize:30,color:'#667467'}}></i></a>
+      <a href='https://www.instagram.com/cyclefevertv/' target='_blank'><i className='fab fa-instagram' style={{marginLeft:20,fontSize:30, color:'#667467'}}></i></a>
+      <a href='https://www.youtube.com/channel/UC5lBCpUunSuN3lCxxTfkunA' target='_blank'><i className='fab fa-youtube' style={{marginLeft:20,fontSize:30,color:'#667467'}}></i></a>
       </div>
     </footer>
   );
