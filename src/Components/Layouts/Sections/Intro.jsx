87feeb615596c86c955ba2c9392114b4c725fad9
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-12-16 02:38:47 -0500

    update name

--- src/Components/Layouts/Sections/Intro.jsx
+++ src/Components/Layouts/Sections/Intro.jsx
@@ -5,7 +5,7 @@ const Intro = () =>{
     <section className='intro'>
       <div className='introTitle'>
         <h1 style={{fontSize:'2em', textAlign:'center', color:'#fff'}}>Bikes, Cars, &amp; Rock N Roll</h1>
-        <h1 style={{textAlign:'center', fontSize:'6em', color:'darkred'}}>CFTv</h1>
+        <h1 style={{textAlign:'center', fontSize:'6em', color:'darkred'}}>CFTV</h1>
         <h1 style={{textAlign:'center', color:'#82b1ff'}}>Join the Adventure &amp; Enjoy the Ride.</h1>
       </div>
     </section>
